dca85fb24b64cdc199a79256a7da70d959ea6aec
Matheus Ale <m.matheus.ale@email.com>
2020-07-05 16:43:12 -0300

    changes

--- apps/mobile/src/pages/Private/pages/Home/components/SpendingForm/WalletSelect.tsx
+++ apps/mobile/src/pages/Private/pages/Home/components/SpendingForm/WalletSelect.tsx
@@ -2,26 +2,37 @@ import { useSelector } from "react-redux";
 import React, { useMemo } from "react";
 import Form, { InputGroupInline, Select, ToggleKeyword } from "@/components/Form";
 import { useFormContext } from "react-hook-form";
+import { IWallet } from '@spending-app/core-types'
+import { Text } from "react-native";
 
 export default () => {
 
   const form = useFormContext();
-  const formValues = form.watch();
+  const formValues = form.watch('wallet');
 
-  const wallets = useSelector(state => state.Wallet.list);
+  const wallets: IWallet[] = useSelector(state => state.Wallet.list);
   const walletOptions = useMemo(() => {
     return wallets.map(w => ({ text: w.name, key: w._id, value: w }))
   }, [wallets]);
 
   return (
     <>
-      <Select
-        label="Pagamento"
-        name="wallet"
-        data={walletOptions}
-      />
+      <InputGroupInline>
+        {wallets.map(wallet => (
+          <ToggleKeyword
+            name="wallet"
+            value={wallet}
+            label={wallet.name}
+            key={wallet._id}
+            style={{ marginRight: 10 }}
 
-      {(formValues && formValues.wallet && formValues.wallet.isCard) && (
+          />
+        ))}
+      </InputGroupInline>
+
+      <Text>{ JSON.stringify(formValues) }</Text>
+
+      {(formValues && formValues.wallet.isCard) && (
         <InputGroupInline>
           <ToggleKeyword
             name="payment"
